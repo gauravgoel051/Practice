6334cea7067ba92a602e90cb794cadd7ab8a18c5
gaurav <gauravgoel051@gmail.com>
2024-04-09 13:48:21 +0530

    method overloading

---
+++ src/Scaler/CallStack.java
@@ -0,0 +1,24 @@
+package Scaler;
+
+public class CallStack {
+    public static void main(String[] args) {
+        int x=0;
+        System.out.println("inside main x = "+ x);
+        first();
+    }
+    static void first(){
+        int x=10;
+        System.out.println("inside first x = "+x);
+        second();
+    }
+    static void second(){
+        int x =20;
+        System.out.println("inside second x = "+x);
+        third();
+    }
+    static void third(){
+        int x = 30;
+        System.out.println("inside third x = "+x);
+    }
+
+}
